8ca2ffa680c173f0afbc0c6559b9605634d70a20
Kaivalya Kate <6522kaivalya@gmail.com>
2021-02-02 23:39:09 +0530

    February30DayCodingChallenge()

--- Data_Structures_Algos/PalindromicSubstrings.java
+++ Data_Structures_Algos/PalindromicSubstrings.java
@@ -5,9 +5,17 @@ public class PalindromicSubstrings {
     public static HashMap<String, Boolean> palindromeMap = new HashMap<>();
 
     public static boolean PalindromeString(String str) {
-        for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+        if (palindromeMap.containsKey(str)) {
+            if (palindromeMap.get(str) == true) {
+                return true;
+            } else if (palindromeMap.get(str) == false) {
                 return false;
+            }
+        } else {
+            for (int i = 0; i < str.length() / 2; i++) {
+                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                    return false;
+                }
             }
         }
         palindromeMap.put(str, true);
@@ -30,7 +38,7 @@ public class PalindromicSubstrings {
     }
 
     public static void main(String[] args) {
-        String A = "a";
+        String A = "XABAX";
         System.out.println(countSubstrings(A));
     }
 }
