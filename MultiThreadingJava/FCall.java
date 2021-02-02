3103efd808f9611241b0c37db562dc1754fcdf90
Kaivalya Kate <6522kaivalya@gmail.com>
2021-02-02 10:16:58 +0530

    Future & Callables

---
+++ MultiThreadingJava/FCall.java
@@ -0,0 +1,63 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FCall {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        Future<Integer> f1;
+        Future<Double> f2;
+
+        f1 = es.submit(new Sum(10));
+        f2 = es.submit(new Hypotenuse(10.34, 15.89));
+
+        try {
+            System.out.println(f1.get());
+            System.out.println(f2.get());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        es.shutdown();
+    }
+}
+
+class Sum implements Callable<Integer> {
+    int n;
+    int sum = 0;
+
+    Sum(int n) {
+        this.n = n;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+}
+
+/**
+ * Hypotenuse implements Runnable
+ */
+class Hypotenuse implements Callable<Double> {
+    Double A, B;
+
+    Hypotenuse(Double A, Double B) {
+        this.A = A;
+        this.B = B;
+    }
+
+    @Override
+    public Double call() throws Exception {
+        // TODO Auto-generated method stub
+        return Math.sqrt(Math.pow(A, 2) + Math.pow(B, 2));
+    }
+
+}
