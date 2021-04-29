c32af1d926c25d7f70543e3c669ade22b763b471
Kaivalya Kate <6522kaivalya@gmail.com>
2021-04-29 20:10:50 +0530

    ValidMountainArray

---
+++ Data_Structures_Algos/MountainArray.java
@@ -0,0 +1,25 @@
+public class MountainArray {
+
+    public boolean validMountainArray(int[] arr) {
+        int N = A.length;
+        int i = 0;
+
+        // walk up
+        while (i + 1 < N && A[i] < A[i + 1])
+            i++;
+
+        // peak can't be first or last
+        if (i == 0 || i == N - 1)
+            return false;
+
+        // walk down
+        while (i + 1 < N && A[i] > A[i + 1])
+            i++;
+
+        return i == N - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MountainArray().validMountainArray(new int[] { 3, 2, 1 }));
+    }
+}
