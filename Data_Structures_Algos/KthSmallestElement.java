e1f39ec995a1dc826075707baf9d82051243a325
Kaivalya Kate <6522kaivalya@gmail.com>
2021-05-11 10:42:25 +0530

    KthSmallestOptimized

--- Data_Structures_Algos/KthSmallestElement.java
+++ Data_Structures_Algos/KthSmallestElement.java
@@ -1,72 +1,64 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class KthSmallestElement {
-    List<Integer> arr = new ArrayList<>();
 
-    public void inOrder(BNode root, int k){
-        if(root==null)
-            return;
-        inOrder(root.left, k);
-        arr.add(root.data);
-        inOrder(root.right, k);
-    } 
-    
-    public int kthSmallest(BNode root, int k) {
-        inOrder(root, k);
-        return arr.get(k-1);
+    public int kthSmallest(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+        while (true) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if (--k == 0) {
+                return root.val;
+            }
+            root = root.right;
+        }
     }
 
     public static void main(String[] args) {
         BinaryTree bst = new BinaryTree();
-        bst.InsertNode(new BNode(5));
-        bst.InsertNode(new BNode(3));
-        bst.InsertNode(new BNode(6));
-        bst.InsertNode(new BNode(2));
-        bst.InsertNode(new BNode(1));
-        bst.InsertNode(new BNode(4));
-        System.out.println(new KthSmallestElement().kthSmallest(bst.root, 1));
+        bst.InsertNode(new TreeNode(5));
+        bst.InsertNode(new TreeNode(3));
+        bst.InsertNode(new TreeNode(6));
+        bst.InsertNode(new TreeNode(2));
+        bst.InsertNode(new TreeNode(1));
+        bst.InsertNode(new TreeNode(4));
+        System.out.println(new KthSmallestElement().kthSmallest(bst.root, 3));
     }
 }
 
-class BinaryTree{
-    BNode root = null;
+class BinaryTree {
+    TreeNode root = null;
 
-    public void InsertNode(BNode node){
-        BNode temp = root;
+    public void InsertNode(TreeNode node) {
+        TreeNode temp = root;
 
-        if(temp==null){
+        if (temp == null) {
             this.root = node;
         } else {
-            while(temp!=null){
-                if(node.data<temp.data){
-                    if(temp.left==null){
+            while (temp != null) {
+                if (node.val < temp.val) {
+                    if (temp.left == null) {
                         temp.left = node;
                         break;
                     } else {
                         temp = temp.left;
                     }
-                } else if(node.data>temp.data){
-                    if(temp.right==null){
+                } else if (node.val > temp.val) {
+                    if (temp.right == null) {
                         temp.right = node;
                         break;
                     } else {
                         temp = temp.right;
                     }
-                } 
+                }
             }
         }
     }
 
 }
 
-class BNode{
-
-    int data;
-    BNode left;
-    BNode right;
-
-    BNode(int data){
-        this.data = data;
-    }
-}
